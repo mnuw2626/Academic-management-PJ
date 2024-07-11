ed5ea0a9f330daa1f3be3b5e77a1da58bc4c194f
mnuw2626 <mnuw2626@naver.com>
2024-07-11 20:21:56 +0900

    Add files via upload

---
+++ src/main/java/com/academic/AcademicManagementPjApplication.java
@@ -0,0 +1,13 @@
+package com.academic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicManagementPjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcademicManagementPjApplication.class, args);
+    }
+
+}
