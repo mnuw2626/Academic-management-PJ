dedda698b32febca2d1a1b8ade3823a16aa5821a
mnuw2626 <mnuw2626@naver.com>
2024-07-22 21:20:31 +0900

    240722_temp

--- src/main/java/com/academic/config/SecurityConfig.java
+++ src/main/java/com/academic/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.academic.config;
 
+import com.academic.handler.LoginSuccessHandler;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,10 +19,11 @@ public class SecurityConfig {
 
         http.formLogin(config -> {
             config.loginPage("/user/login") //내가 쓰는 로그인 페이지 GETMAPPING경로
-            .loginProcessingUrl("/user/login") //로그인을 시도하는 POST Mapping 경로
+//            .loginProcessingUrl("/user/login") //로그인을 시도하는 POST Mapping 경로
             .usernameParameter("id") //로그인에서 아이디 input의 name값
-            .passwordParameter("password") //비밀번호 input의 name값
-            .defaultSuccessUrl("/main") //로그인 성공시 이동할 getmapping경로
+//            .passwordParameter("password") //비밀번호 input의 name값
+//            .defaultSuccessUrl("/main") //로그인 성공시 이동할 getmapping경로
+            .successHandler(new LoginSuccessHandler())
             .permitAll();
 
         });
--- src/main/java/com/academic/handler/LoginSuccessHandler.java
+++ src/main/java/com/academic/handler/LoginSuccessHandler.java
@@ -5,11 +5,13 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
 
+@Log4j2
 public class LoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
