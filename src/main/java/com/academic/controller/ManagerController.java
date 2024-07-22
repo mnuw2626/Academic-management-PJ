f6bb548ba61fd2f4bc3a30bba2b8d92bae4dfde2
mnuw2626 <mnuw2626@naver.com>
2024-07-22 21:41:24 +0900

    240722_로그인 기능 구현

--- src/main/java/com/academic/controller/ManagerController.java
+++ src/main/java/com/academic/controller/ManagerController.java
@@ -6,9 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/manager")
 public class ManagerController {
-    @Autowired
 
     @GetMapping("add_std")
     public void get_add_std(){
