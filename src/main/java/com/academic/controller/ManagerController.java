0be8705ebddd6d56a832e041da89cf3611c2c18f
yangjunsik <wnstlr1379@naver.com>
2024-07-23 16:57:14 +0900

    학생등록

--- src/main/java/com/academic/controller/ManagerController.java
+++ src/main/java/com/academic/controller/ManagerController.java
@@ -1,18 +1,27 @@
 package com.academic.controller;
 
+import com.academic.dto.StdDTO;
+import com.academic.service.ManagerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("/manage")
 public class ManagerController {
     @Autowired
+    ManagerService managerService;
 
-    @GetMapping("add_std")
+    @GetMapping("/add_std")
     public void get_add_std(){
-        System.out.println("학생등록시도");
+    }
 
+    @PostMapping("/add_std")
+    public void post_add_std(StdDTO stdDTO){
+        System.out.println("학생등록시도");
+        managerService.manager_add_std(stdDTO);
+        System.out.println("학생등록성공");
     }
 }
